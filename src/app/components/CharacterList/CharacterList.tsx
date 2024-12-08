6f7083683f88593aa0e2bb402ab17147d08ee0cb
Raúl Pérez <jrpcassone@gmail.com>
2024-12-08 23:02:26 +0100

    feat: implement character picker

--- src/app/components/CharacterList/CharacterList.tsx
+++ src/app/components/CharacterList/CharacterList.tsx
@@ -1,21 +1,26 @@
 import { Character } from "@/app/interfaces/chracter";
 import CharacterCard from "../CharacterCard/CharacterCard";
 import styles from "@/app/components/CharacterList/characterList.module.css";
-interface ChractersListProps {
+
+interface CharactersListProps {
   characters: Character[];
+  onPickCharacter: (character: Character) => void;
 }
 
-const CharacterList = ({ characters }: ChractersListProps) => {
+const CharacterList = ({
+  characters,
+  onPickCharacter,
+}: CharactersListProps) => {
   return (
-    <section>
+    <>
       <ul className={styles.characterList}>
-        {characters.map((character, index) => (
-          <li key={index}>
+        {characters.map((character) => (
+          <li key={character.id} onClick={() => onPickCharacter(character)}>
             <CharacterCard character={character} />
           </li>
         ))}
       </ul>
-    </section>
+    </>
   );
 };
 
